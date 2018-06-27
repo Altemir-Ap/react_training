25a43c52da650123a2b6bb75578c9279837a84c5
Altemir <altemir.git@gmail.com>
2018-06-27 11:14:55 +0100

    Adding first transiction

--- react_transitions/03_transitions/src/components/Transition.js
+++ react_transitions/03_transitions/src/components/Transition.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import Transition from 'react-transition-group/Transition';
 
 class TransitionComp extends Component{
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true : false
+        }) 
+    }
 
     render(){
         return(
             <div>
-                Transition
+                
+                <Transition 
+                in={this.state.show}
+                timeout={{
+                    enter:2000,
+                    exit:50
+                }}
+
+                enter={true}
+                exit={true}
+
+
+                >
+                
+               { state => 
+                    <div className={`square square-${state}`}>
+                        {`square square-${state}`}
+                    </div> 
+                }
+                </Transition>
+                <div className="showDiv"
+                    onClick={this.showDiv}
+                    >
+                    Show or hide
+                </div>    
             </div>
 
         )
