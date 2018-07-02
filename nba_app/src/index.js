dd767b417f69790930bc97aa7b3c5aa1452e5b1b
Altemir <altemir.git@gmail.com>
2018-07-02 23:37:04 +0100

    Add components

--- nba_app/src/index.js
+++ nba_app/src/index.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
+import Routes from './routes'
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes/>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
